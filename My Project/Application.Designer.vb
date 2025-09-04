ad75f6f60650021d3b2bc37040f4442fbb7bec37
MANOLOV02 <MANOLOV02>
2025-09-04 20:19:47 -0300

    Updated

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Ba2_Bsa_Manager.Form1
+            Me.MainForm = Global.Ba2_Bsa_Manager.Mainform_form
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
